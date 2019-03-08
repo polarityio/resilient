4bd0b9667a5ae15f86827432e2c194e7fd1a382c
Ed <ed@polarity.io>
2019-03-07 20:16:29 -0500

    Rename as IBM Resilient

--- config/config.js
+++ config/config.js
@@ -1,133 +1,130 @@
 module.exports = {
-    /**
-     * Name of the integration which is displayed in the Polarity integrations user interface
-     *
-     * @type String
-     * @required
-     */
-    name: "Resilient",
-    /**
-     * The acronym that appears in the notification window when information from this integration
-     * is displayed.  Note that the acronym is included as part of each "tag" in the summary information
-     * for the integration.  As a result, it is best to keep it to 4 or less characters.  The casing used
-     * here will be carried forward into the notification window.
-     *
-     * @type String
-     * @required
-     */
-    acronym: "RES",
-    /**
-     * Description for this integration which is displayed in the Polarity integrations user interface
-     *
-     * @type String
-     * @optional
-     */
-    description: "Query information from IBM Resilient platform",
-    entityTypes: ['IPv4', 'domain', 'email', 'hash'],
-    /**
-     * An array of style files (css or less) that will be included for your integration. Any styles specified in
-     * the below files can be used in your custom template.
-     *
-     * @type Array
-     * @optional
-     */
-    "styles": [
-        "./styles/resilient.less"
-    ],
-    /**
-     * Provide custom component logic and template for rendering the integration details block.  If you do not
-     * provide a custom template and/or component then the integration will display data as a table of key value
-     * pairs.
-     *
-     * @type Object
-     * @optional
-     */
-    block: {
-        component: {
-            file: "./components/block.js"
-        },
-        template: {
-            file: "./templates/block.hbs"
-        }
+  /**
+   * Name of the integration which is displayed in the Polarity integrations user interface
+   *
+   * @type String
+   * @required
+   */
+  name: 'IBM Resilient',
+  /**
+   * The acronym that appears in the notification window when information from this integration
+   * is displayed.  Note that the acronym is included as part of each "tag" in the summary information
+   * for the integration.  As a result, it is best to keep it to 4 or less characters.  The casing used
+   * here will be carried forward into the notification window.
+   *
+   * @type String
+   * @required
+   */
+  acronym: 'RES',
+  /**
+   * Description for this integration which is displayed in the Polarity integrations user interface
+   *
+   * @type String
+   * @optional
+   */
+  description: 'Query incidents within the IBM Resilient Incident Response Platform',
+  entityTypes: ['IPv4', 'domain', 'email', 'hash'],
+  /**
+   * An array of style files (css or less) that will be included for your integration. Any styles specified in
+   * the below files can be used in your custom template.
+   *
+   * @type Array
+   * @optional
+   */
+  styles: ['./styles/resilient.less'],
+  /**
+   * Provide custom component logic and template for rendering the integration details block.  If you do not
+   * provide a custom template and/or component then the integration will display data as a table of key value
+   * pairs.
+   *
+   * @type Object
+   * @optional
+   */
+  block: {
+    component: {
+      file: './components/block.js'
     },
-    summary: {
-        component: {
-            file: './components/summary.js'
-        },
-        template: {
-            file: './templates/summary.hbs'
-        }
+    template: {
+      file: './templates/block.hbs'
+    }
+  },
+  summary: {
+    component: {
+      file: './components/summary.js'
     },
-    request: {
-        // Provide the path to your certFile. Leave an empty string to ignore this option.
-        // Relative paths are relative to the STAXX integration's root directory
-        cert: '',
-        // Provide the path to your private key. Leave an empty string to ignore this option.
-        // Relative paths are relative to the STAXX integration's root directory
-        key: '',
-        // Provide the key passphrase if required.  Leave an empty string to ignore this option.
-        // Relative paths are relative to the STAXX integration's root directory
-        passphrase: '',
-        // Provide the Certificate Authority. Leave an empty string to ignore this option.
-        // Relative paths are relative to the STAXX integration's root directory
-        ca: '',
-        // An HTTP proxy to be used. Supports proxy Auth with Basic Auth, identical to support for
-        // the url parameter (by embedding the auth info in the uri)
-        proxy: '',
-        /**
-         * If set to false, the integeration will ignore SSL errors.  This will allow the integration to connect
-         * to STAXX servers without valid SSL certificates.  Please note that we do NOT recommending setting this
-         * to false in a production environment.
-         */
-        rejectUnauthorized: false
-    },
-    logging: {
-        level: 'info',  //trace, debug, info, warn, error, fatal
-    },
+    template: {
+      file: './templates/summary.hbs'
+    }
+  },
+  request: {
+    // Provide the path to your certFile. Leave an empty string to ignore this option.
+    // Relative paths are relative to the integration's root directory
+    cert: '',
+    // Provide the path to your private key. Leave an empty string to ignore this option.
+    // Relative paths are relative to the integration's root directory
+    key: '',
+    // Provide the key passphrase if required.  Leave an empty string to ignore this option.
+    // Relative paths are relative to the integration's root directory
+    passphrase: '',
+    // Provide the Certificate Authority. Leave an empty string to ignore this option.
+    // Relative paths are relative to the integration's root directory
+    ca: '',
+    // An HTTP proxy to be used. Supports proxy Auth with Basic Auth, identical to support for
+    // the url parameter (by embedding the auth info in the uri)
+    proxy: '',
     /**
-     * Options that are displayed to the user/admin in the Polarity integration user-interface.  Should be structured
-     * as an array of option objects.
-     *
-     * @type Array
-     * @optional
+     * If set to false, the integeration will ignore SSL errors.  This will allow the integration to connect
+     * to STAXX servers without valid SSL certificates.  Please note that we do NOT recommending setting this
+     * to false in a production environment.
      */
-    "options": [
-        {
-            "key": "url",
-            "name": "Resilient URL",
-            "description": "Your Resilient URL",
-            "default": "",
-            "type": "text",
-            "userCanEdit": true,
-            "adminOnly": false
-        },
-        {
-            "key": "username",
-            "name": "Resilient Username",
-            "description": "Your Resilient username.",
-            "default": "",
-            "type": "text",
-            "userCanEdit": true,
-            "adminOnly": false
-        },
-        {
-            "key": "password",
-            "name": "Resilient Password",
-            "description": "Your Resilient password.",
-            "default": "",
-            "type": "password",
-            "userCanEdit": true,
-            "adminOnly": false
-        },
-        {
-            "key": "orgId",
-            "name": "Resilient Org ID: ",
-            "description": "Your Resilient Org ID",
-            "default": "",
-            "type": "text",
-            "userCanEdit": true,
-            "adminOnly": false
-        }
-
-    ]
+    rejectUnauthorized: false
+  },
+  logging: {
+    level: 'info' //trace, debug, info, warn, error, fatal
+  },
+  /**
+   * Options that are displayed to the user/admin in the Polarity integration user-interface.  Should be structured
+   * as an array of option objects.
+   *
+   * @type Array
+   * @optional
+   */
+  options: [
+    {
+      key: 'url',
+      name: 'Resilient URL',
+      description: 'Your Resilient URL',
+      default: '',
+      type: 'text',
+      userCanEdit: true,
+      adminOnly: false
+    },
+    {
+      key: 'username',
+      name: 'Resilient Username',
+      description: 'Your Resilient username.',
+      default: '',
+      type: 'text',
+      userCanEdit: true,
+      adminOnly: false
+    },
+    {
+      key: 'password',
+      name: 'Resilient Password',
+      description: 'Your Resilient password.',
+      default: '',
+      type: 'password',
+      userCanEdit: true,
+      adminOnly: false
+    },
+    {
+      key: 'orgId',
+      name: 'Resilient Org ID: ',
+      description: 'Your Resilient Org ID',
+      default: '',
+      type: 'text',
+      userCanEdit: true,
+      adminOnly: false
+    }
+  ]
 };
