bb79431a33ea34b5063721b52109559c877455b1
Ed <ed@polarity.io>
2019-03-07 20:16:03 -0500

    Display severity rather than search score as a tag

--- components/summary.js
+++ components/summary.js
@@ -2,12 +2,14 @@
 
 polarity.export = PolarityComponent.extend({
   details: Ember.computed.alias('block.data.details'),
-  otherData: Ember.computed('details', function() {
+  tags: Ember.computed('details', function() {
     let data = Ember.A();
     this.get('details.incidents').forEach(function(incident) {
       data.push(incident.type_id);
       data.push(incident.inc_name);
-      data.push(incident.score);
+      if (incident.result && incident.result.severity_code && incident.result.severity_code.name) {
+        data.push('Severity: ' + incident.result.severity_code.name);
+      }
     });
     return data;
   })
